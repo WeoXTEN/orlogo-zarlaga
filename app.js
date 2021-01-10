22fa0bb458bc28d7162038914c0b48fca9924f8c
Weo <nicefreezer@gmail.com>
2021-01-10 14:27:05 +0800

    event listener uud ogloo

---
+++ app.js
@@ -0,0 +1,25 @@
+//Delgetstei ajillah controller
+var uiController = (function () {})();
+//Sanhuutei ajillah controller
+var financeController = (function () {})();
+//Program holboh controller
+var appController = (function (fnCtrl, uiCtrl) {
+  var ctrlAddItem = function () {
+    // 1. Oruulah ogogdliig delgetsees olj avna
+    console.log("Delgetsnees ogogdoliig avah heseg");
+    // 2. Olj avsan ogogdluudee sanhuugiin controllert damjuulj tend hadgalna
+    // 3. Olj avsan ogogdluudee web deeree tohiroh hesegt gargaj ogno
+    // 4. Tosviig tootsno
+    // 5. Etssiin uldegdel , tootsoog delgetsend gargana
+  };
+
+  document.querySelector(".add__btn").addEventListener("click", function () {
+    ctrlAddItem();
+  });
+
+  document.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13) {
+      ctrlAddItem();
+    }
+  });
+})(financeController, uiController);
